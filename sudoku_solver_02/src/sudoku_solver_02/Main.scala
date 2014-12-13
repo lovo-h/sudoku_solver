1eacb44a0570bcd6369ad6ef54f106b77374ba93
Hector <lovohh@gmail.com>
2014-12-13 18:59:42 -0500

    can print all necessary info

--- sudoku_solver_02/src/sudoku_solver_02/Main.scala
+++ sudoku_solver_02/src/sudoku_solver_02/Main.scala
@@ -9,20 +9,25 @@ object Main {
       "000 000 000",
       "000 000 000",
       "000 000 000",
+
       "000 000 000",
       "000 000 000",
       "000 000 000",
+
+      "000 123 456",
       "000 000 000",
-      "000 000 000",
-      "000 000 000");
-    
+      "123 000 000");
+
+    System.out.println("INITIALIZED BOARD");
     var ss = new Sudoku_Solver();
-    ss.print(rows);
-    
-    
+    ss.print_board(rows);
+    System.out.println("ROW: 9");
+    ss.print_row(rows, 8);
+    System.out.println("COL: 2");
+    ss.print_col(rows, 1);
+    System.out.println("SECT: 8");
+    ss.print_sect(rows, 7)
 
   }
-
-
 
 }
--- sudoku_solver_02/src/sudoku_solver_02/Sudoku_Solver.scala
+++ sudoku_solver_02/src/sudoku_solver_02/Sudoku_Solver.scala
@@ -2,14 +2,37 @@ package sudoku_solver_02
 
 class Sudoku_Solver {
   // print array of strings
-  def print(sb: Array[String]): Unit = {
-    for (i <- 0 until sb.length) {
-
+  def print_board(sb: Array[String]): Unit = {
+    for (i <- 0 until 9) {
       System.out.println(sb(i));
-
+      // add filler to delineate sections  
       if ((i + 1) % 3 == 0) {
         System.out.println();
       }
     }
   }
+
+  def print_col(sb: Array[String], col: Int): Unit = {
+    for (i <- 0 until 9) {
+      System.out.println(sb(i).substring(col, col + 1));
+    }
+  }
+
+  def print_row(sb: Array[String], row: Int): Unit = {
+    System.out.println(sb(row));
+  }
+
+  def print_sect(sb: Array[String], sect: Int): Unit = {
+    val rs = if (sect > 5) 6 else (if (sect < 3) 0 else 3); // row start
+    val re = rs + 3; // row end
+    val col = (sect % 3) * 3; // determine column adjustment - due to spaces
+    val cs = if (col > 5) 8 else (if (col > 2) 4 else 0); // col start
+    val ce = cs + 3; // col end
+
+    for (row <- rs until re) {
+      System.out.println(sb(row).substring(cs, ce));
+    }
+
+  }
+
 }
