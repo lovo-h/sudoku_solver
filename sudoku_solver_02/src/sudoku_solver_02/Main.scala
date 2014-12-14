6b8f6ca6263fd0249c83060aea5c5d9aba264ade
Hector <lovohh@gmail.com>
2014-12-14 18:14:15 -0500

    Almost fully working

--- sudoku_solver_02/src/sudoku_solver_02/Main.scala
+++ sudoku_solver_02/src/sudoku_solver_02/Main.scala
@@ -7,10 +7,8 @@ object Main {
   def main(args: Array[String]): Unit = {
     var puzzle = "...........5....9...4....1.2....3.5....7.....438...2......9.....1.4...6..........";
     var ss = new Sudoku_Solver();
-
-    ss.initializeBoard(puzzle);
-    ss.start();
-    ss.setup_relations();
+    ss.processPuzzleInput(puzzle);
+    printGameBoard(ss.getBoard);
 
     //    System.out.println("BOARD:");
     //    printGameBoard(ss.getBoard());
@@ -27,8 +25,8 @@ object Main {
   def printGameBoard(board: collection.mutable.Map[String, String]): Unit = {
     for (row <- 0 until 9) {
       for (col <- 0 until 9) {
-        System.out.print(board(row.toString + col.toString));
-        if ((col + 1) % 3 == 0) System.out.print(" ");
+        System.out.print(board(row.toString + col.toString) + " ");
+        if ((col + 1) % 3 == 0) System.out.print("  ");
       }
       if (row == 2 || row == 5) System.out.println();
       System.out.println();
--- sudoku_solver_02/src/sudoku_solver_02/Sudoku_Solver.scala
+++ sudoku_solver_02/src/sudoku_solver_02/Sudoku_Solver.scala
@@ -3,9 +3,10 @@ package sudoku_solver_02
 class Sudoku_Solver {
   val board = collection.mutable.Map[String, String]();
   val relations = collection.mutable.Map[String, collection.mutable.Map[String, collection.mutable.Map[String, String]]]();
+  var dPlacesCount = 0;
+  var dPlaces = "";
 
-  def initializeBoard(puzzle: String): Unit = {
-    var chars = puzzle.toCharArray();
+  def initializeBoard(): Unit = {
     for (row <- 0 until 9) {
       for (col <- 0 until 9) {
         board += (row.toString + col.toString -> "123456789");
@@ -15,21 +16,21 @@ class Sudoku_Solver {
     setup_relations();
   }
 
-  def start(): Unit = {
-
-  }
-
-  def processPuzzleInput(puzzle: String): Boolean = {
+  def processPuzzleInput(puzzle: String): Unit = {
     var arrPuzzle = puzzle.toCharArray();
     var col = 0;
     var row = 0;
 
+    var isValidSolution: Boolean = true;
+
+    initializeBoard();
+
     for (i <- 0 until arrPuzzle.length) {
       if (arrPuzzle(i).toString.compareTo(".") == 0 || arrPuzzle(i).toString.compareTo("0") == 0) {
-        System.out.println("Skip");
+        //        System.out.println("Skip: " + i.toString);
       } else {
         if (!successfullySetElement(row.toString + col.toString, arrPuzzle(i).toString)) {
-          false;
+          isValidSolution = false;
         }
       }
       col += 1;
@@ -38,18 +39,82 @@ class Sudoku_Solver {
         row += 1;
       }
     }
-    true;
+
+    if (isValidSolution) {
+      // verify solution
+    } else {
+      // return empty board
+    }
+
+    // return board when finished: return board
+
+  }
+
+  def solveCurrentBoard() {
+    // if all elements have a single solution, it's already valid
+    if (isCurrentBoardSolutionValid()) {
+      // return true or return board
+    }
+
+    var minElementVal = "123456789";
+    var location = "00";
+
+    for (row <- 0 until 9) {
+      for (col <- 0 until 9) {
+        var element = row.toString + col.toString;
+        var elementVal = board(element);
+        if (elementVal.length > 1) {
+          if (elementVal.length < minElementVal.length) {
+            minElementVal = elementVal;
+            location = element;
+          }
+        }
+      }
+    }
+
+    // just in case something goes wrong, we backup
+    var backupCurrentBoard = board.clone;
+    var arrPossibleSolutions = board(location).toCharArray();
+    var isPossibleNow: Boolean = false;
+
+    for (sol <- 0 to arrPossibleSolutions.length - 1) {
+      // TODO: WORKING HERE
+      if (successfullySetElement(location, arrPossibleSolutions(sol).toString)) {
+        
+      } else {
+        
+      }
+    }
+
+  }
+
+  def isCurrentBoardSolutionValid(): Boolean = {
+    board.foreach { element =>
+      if (board(element._1).length() != 1) {
+        return false;
+      }
+    };
+    return true;
   }
 
   def successfullySetElement(element: String, value: String): Boolean = {
-    val arrRemainingValuesofElement = board(element).toCharArray();
+    val remainingValuesofElement = board(element).replace(value, "");
+    val arrRemainingValuesofElement = remainingValuesofElement.toCharArray();
 
-    for (ele <- 0 to arrRemainingValuesofElement.length) {
+    if (arrRemainingValuesofElement.length == 0)
+      return true;
+
+    for (ele <- 0 to arrRemainingValuesofElement.length - 1) {
       if (!deducePuzzle(element, arrRemainingValuesofElement(ele).toString)) {
-        false;
+        return false;
       }
     }
-    true;
+    return true;
+  }
+
+  // TODO: temporary, remove
+  def print(msg: String) {
+    System.out.println(msg);
   }
 
   def deducePuzzle(element: String, delVal: String): Boolean = {
@@ -57,7 +122,7 @@ class Sudoku_Solver {
     var bakCurrentEleVal = currentEleVal;
 
     if (!currentEleVal.contains(delVal)) { // if it's not in here, we cannot remove it
-      true;
+      return true;
     }
 
     currentEleVal = currentEleVal.replace(delVal, "");
@@ -65,27 +130,62 @@ class Sudoku_Solver {
 
     if (currentEleVal.length == 0) {
       board(element) = bakCurrentEleVal; // abort, something went wrong
-      false;
+      return false;
     } else if (currentEleVal.length == 1) {
       var d2 = currentEleVal; // TODO: check if replaceable by cEV
       var all_relations = relations(element)("all"); // key-value map
       all_relations.foreach { key =>
         if (!deducePuzzle(key._1, d2)) {
-          false;
+          return false;
         }
       };
     }
-    // Working Here: need to add one final piece of logic
-    true;
+    var row_relations = relations(element)("rows"); // key-value map
+    var col_relations = relations(element)("cols"); // key-value map
+    var sect_relations = relations(element)("sects"); // key-value map
+
+    if (!isValidInGroupRelation(row_relations, delVal, element, "ROW")) {
+      return false;
+    }
+    if (!isValidInGroupRelation(col_relations, delVal, element, "COL")) {
+      return false;
+    }
+    if (!isValidInGroupRelation(sect_relations, delVal, element, "SECT")) {
+      return false;
+    }
+
+    return true;
+  }
+
+  def isValidInGroupRelation(type_relations: collection.mutable.Map[String, String], delVal: String, eleTest: String, relationType: String): Boolean = {
+    dPlaces = "";
+    dPlacesCount = 0;
+
+    type_relations.foreach { key =>
+      if (board(key._1).contains(delVal)) {
+        dPlaces = key._1;
+        dPlacesCount += 1;
+      }
+    };
+
+    if (dPlacesCount == 0) {
+      return false;
+    } else if (dPlacesCount == 1) {
+      if (!successfullySetElement(dPlaces, delVal)) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   def setup_relations() {
     for (row <- 0 until 9) {
       for (col <- 0 until 9) {
         val sectInt = convertRowColToSect(row, col);
-        val rows = getRow(row);
-        val cols = getCol(col);
-        val sects = getSect(sectInt);
+        val rows = getRow(row, col);
+        val cols = getCol(col, row);
+        val sects = getSect(sectInt, row, col);
         val all_relations = rows ++ cols ++ sects;
 
         val mapElementAttrs = collection.mutable.Map[String, collection.mutable.Map[String, String]]();
@@ -132,23 +232,25 @@ class Sudoku_Solver {
     board;
   }
 
-  def getRow(row: Int): collection.mutable.Map[String, String] = {
+  def getRow(row: Int, colInt: Int): collection.mutable.Map[String, String] = {
     val rows = collection.mutable.Map[String, String]();
     for (col <- 0 until 9) {
-      rows += (row.toString + col.toString -> board(row.toString + col.toString));
+      if (col != colInt)
+        rows += (row.toString + col.toString -> board(row.toString + col.toString));
     }
     rows;
   }
 
-  def getCol(col: Int): collection.mutable.Map[String, String] = {
+  def getCol(col: Int, rowInt: Int): collection.mutable.Map[String, String] = {
     val cols = collection.mutable.Map[String, String]();
     for (row <- 0 until 9) {
-      cols += (row.toString + col.toString -> board(row.toString + col.toString));
+      if (row != rowInt)
+        cols += (row.toString + col.toString -> board(row.toString + col.toString));
     }
     cols;
   }
 
-  def getSect(sectInt: Int): collection.mutable.Map[String, String] = {
+  def getSect(sectInt: Int, rowInt: Int, colInt: Int): collection.mutable.Map[String, String] = {
     val sect = collection.mutable.Map[String, String]();
 
     val rs = if (sectInt > 5) 6 else if (sectInt < 3) 0 else 3;
@@ -158,7 +260,8 @@ class Sudoku_Solver {
 
     for (row <- rs until re) {
       for (col <- cs until ce) {
-        sect += (row.toString + col.toString -> board(row.toString + col.toString));
+        if (row != rowInt || col != colInt)
+          sect += (row.toString + col.toString -> board(row.toString + col.toString));
       }
     }
 
